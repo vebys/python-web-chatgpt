280bdaf0e6ae0664a1cc3c870af5b84d83afb112
vebys-office <vebysl@qq.com>
2023-02-23 22:22:05 +0800

    init

--- openai_test/settings.py
+++ openai_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$1)ymw5$m!x#mxo2d*_utlif+-6$o!-944gf6yc+fmphgzsmik
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
