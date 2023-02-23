9c2df017fdd99b7bd645f67fb14827e26e12b349
vebys-office <vebysl@qq.com>
2023-02-23 22:06:57 +0800

    init

---
+++ chatgpt_test/views.py
@@ -0,0 +1,36 @@
+from django.views.generic import View
+from django.shortcuts import render
+from django.http.response import JsonResponse
+import openai
+
+class Chat(View):
+  def get(self,request):
+      return render(request,'openai_test/chat.html')
+
+
+  def post(self,request):
+    ask = request.POST['ask']
+    # print(ask)
+    openai.api_key = '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'
+    response = openai.Completion.create(
+      model='text-davinci-003',
+      prompt=ask,
+      temperature=0.6,
+      max_tokens=2048,
+      top_p=1.0,
+      frequency_penalty=1.0,
+      presence_penalty=1.0,
+    )
+    # print(response)
+    print(response.choices)
+    print(response.choices[0].text)
+    return JsonResponse({'code':0,'msg':response.choices[0].text})
+
+
+
+
+
+
+
+
+
